4e996af7afbd66ce28058387820f09d02e603c30
VladNekula <158812345+VladNekula@users.noreply.github.com>
2025-08-22 15:54:17 +0300

    зациклил

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -1,38 +1,56 @@
-#include <TXLib.h>
+#include <TXLib.h>  //Проверка ввода, вывода, зациклить,  общие вычисления
 #include <stdio.h>
 #include <math.h>
+
+const double EPSILON = 0.00001;
+
+double Discriminant(double a, double b, double c);
+void korni(double a, double b, double c);
+bool isEqual(double number1, double number2);
+
+int main() {
+    double a = NAN;
+    double b = NAN;
+    double c = NAN;
+
+    do {
+        printf("Введите a, b и c (чтобы выйти из цикла, введите \'e\'): ");
+        int tmp = scanf("%lf %lf %lf", &a, &b, &c);
+        if (tmp != 3) {
+            printf("Некорректный формат ввода\n");
+        }
+        else {
+            korni(a, b, c);
+        }
+    } while (getchar() != 'e');
+    printf("Вы вышли из цикла");
+    return 0;
+}
 
 double Discriminant(double a, double b, double c) {
     return b * b - 4.0 * a * c;
 }
 
+bool isEqual(double number1, double number2) {
+    return fabs(number2 - number1) < EPSILON;
+}
 
-
-int korni(double a, double b, double c) {
-    int D = Discriminant(a, b, c);
+void korni(double a, double b, double c) {
+    double D = Discriminant(a, b, c);
     if (D < 0) {
-        printf("РќРµС‚ РєРѕСЂРЅРµР№\n");
-        return 0;
+        printf("Нет корней\n");
     }
-    else if (D == 0) {
+    else if (isEqual(D, (double) 0)) {
         double x = -b / (2.0 * a);
-        printf("x = %0.2lf", x);
-        return 0;
+        printf("x = %g\n", x);
     }
     else {
-        double x1 = (-b - sqrt(Discriminant(a, b, c))) / (2.0 * a);
-        double x2 = (-b + sqrt(Discriminant(a, b, c))) / (2.0 * a);
-        printf("x1 = %0.2lf\nx2 = %0.2lf", x1, x2);
-        return 0;
+        double n1 = - b / (2.0 * a);
+        double n2 = sqrt(D) / (2.0 * a);
+
+        double x1 = n1 - n2;
+        double x2 = n1 + n2;
+        printf("x1 = %g\nx2 = %g\n", x1, x2);
     }
 }
-
-int main() {
-    double a, b, c;
-    printf("Р’РІРµРґРёС‚Рµ a, b Рё c: ");
-    scanf("%lf %lf %lf", &a, &b, &c);
-    korni(a, b, c);
-    return 0;
-}
-
 
